35d2fc42464cbccef2e544126e01d641888ac3dc
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-11 19:47:40 +0000

    able to instantiate the clusterer and stream
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clusterer.R
+++ pkg/R/clusterer.R
@@ -6,19 +6,36 @@ clusterer <- function(algorithm="denstream") {
 }
 
 ## show
-#setMethod("show", signature(object = "clusterer"),
-#        function(object) {
-#            cat("Clusterer\n", 
-#                    "Algorithm:", object@algorithm, "\n",
-#                    "Stream:", object@stream, "\n"
-#                    )
-#            invisible(NULL)
-#        }
-#)
+setMethod("show", signature(object = "clusterer"),
+        function(object) {
+            cat("Clusterer\n", 
+                    "Algorithm:", object@algorithm, "\n"
+                    )
+            invisible(NULL)
+        }
+)
 
-cluster <- function() {
-	.jpackage("ClusterDS", jars="*")
-	c = .jnew("moa/clusterers/denstream/DenStream")
+cluster <- function(clusterer) {
+	if (clusterer@algorithm == "denstream") {
+	   .jpackage("ClusterDS", jars="*")
+
+	   c <- .jnew("moa/clusterers/denstream/DenStream")
+	   .jcall(c, "V", "prepareForUse")
+
+	   #RandomRBFGeneratorEvents extends abstract class ClusteringStream
+	   strm <- .jnew("moa/streams/clustering/RandomRBFGeneratorEvents")
+	   .jcall(strm, "V", "prepareForUse")
+	   
+	   #looping through the stream, feeding the new datapoints into 
+	   #the algorithm
+	   for (i in 1:1000) {
+
+	      #TODO: this call isn't working, but when calling
+	      # .jmethods(strm), the method signature does indeed exist
+	      inst <- .jcall(strm, "Lweka/core/Instance", "nextInstance")
+	      .jcall(c, "V", "trainOnInstanceImpl", inst)
+           }
+	}
 
 }
 
