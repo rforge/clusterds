c3f617cca04493130b3e35b9d9c808465643a31d
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-12 20:35:14 +0000

    able to create a stream and begin clustering
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clusterer.R
+++ pkg/R/clusterer.R
@@ -16,26 +16,44 @@ setMethod("show", signature(object = "clusterer"),
 )
 
 cluster <- function(clusterer) {
-	if (clusterer@algorithm == "denstream") {
-	   .jpackage("ClusterDS", jars="*")
+   if (clusterer@algorithm == "denstream") {
+      c <- .jnew("moa/clusterers/denstream/DenStream")
+      .jcall(c, "V", "prepareForUse")
+   }
 
-	   c <- .jnew("moa/clusterers/denstream/DenStream")
-	   .jcall(c, "V", "prepareForUse")
+   else if (clusterer@algorithm == "clustream") {
+      c <- .jnew("moa/clusterers/clustream/Clustream")
+      .jcall(c, "V", "prepareForUse")
+   }
 
-	   #RandomRBFGeneratorEvents extends abstract class ClusteringStream
-	   strm <- .jnew("moa/streams/clustering/RandomRBFGeneratorEvents")
-	   .jcall(strm, "V", "prepareForUse")
-	   
-	   #looping through the stream, feeding the new datapoints into 
-	   #the algorithm
-	   for (i in 1:1000) {
+   else if (clusterer@algorithm == "cobweb") {
+      c <- .jnew("moa/clusterers/CobWeb")
+      .jcall(c, "V", "prepareForUse")
+   }
 
-	      #TODO: this call isn't working, but when calling
-	      # .jmethods(strm), the method signature does indeed exist
-	      inst <- .jcall(strm, "Lweka/core/Instance", "nextInstance")
-	      .jcall(c, "V", "trainOnInstanceImpl", inst)
-           }
-	}
+   else {
+      return;
+   }
+
+   panel <- .jnew("moa/gui/clustertab/ClusteringAlgoPanel")
+   strm <- .jcall(panel, "Lmoa/streams/clustering/ClusteringStream;", "getStream")
+   .jcall(strm, "V", "prepareForUse")
+
+   #RandomRBFGeneratorEvents extends abstract class ClusteringStream
+   #strm <- .jnew("moa/streams/clustering/RandomRBFGeneratorEvents")
+   #.jcall(strm, "V", "prepareForUse")
+   
+   #looping through the stream, feeding the new datapoints into 
+   #the algorithm
+   for (i in 1:1000) {
+
+      #TODO: this call isn't working, but when calling
+      # .jmethods(strm), the method signature does indeed exist
+      inst <- .jnew("weka/core/Instance")
+      inst <- .jcall(strm, "Lweka/core/Instance;", "nextInstance")
+      .jcall(c, "V", "trainOnInstanceImpl", inst)
+   }
+	
 
 }
 
