a5367dc708c30c153613c9b809f1fc3211e4e6ba
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-25 17:26:44 +0000

    added the capability to define parameters via cli parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clusterer.R
+++ pkg/R/clusterer.R
@@ -1,4 +1,3 @@
-
 ## constructor
 clusterer <- function(algorithm="denstream") {
 
@@ -15,8 +14,17 @@ setMethod("show", signature(object = "clusterer"),
         }
 )
 
-cluster <- function(clusterer) {
+cluster <- function(clusterer, options="") {
    if (clusterer@algorithm == "denstream") {
+      # denstream options:
+      # -e epsilon 	0.01 (defines the epsilon neighborhood, range: 0 to 1)
+      # -p minPoints 	10 (min. num. points a cluster must have)
+      # -l lambda	0.006 (range: 0 to 1)
+      # -b beta		0.001 (range: 0 to 1)
+      # -m mu		1 (range: 0 to max(double))
+      # -i initPoints	10000 (number of points to use for initialization)
+      # -M		false (evaluate micro clustering flag)
+
       c <- .jnew("moa/clusterers/denstream/DenStream")
       .jcall(c, "V", "prepareForUse")
    }
@@ -35,20 +43,24 @@ cluster <- function(clusterer) {
       return;
    }
 
+   # if the user passes options to the function, the cluster's parameters can be set
+   if (options != "") {
+      o <- .jcall(c, "Lmoa/options/Options;", "getOptions")
+      .jcall(o, "V", "setViaCLIString", options)
+      test <- .jcall(o, "Ljava/lang/String;", "getAsCLIString")
+      print(test)
+   }
+   else {
+      print("using default options")
+   }
+
    panel <- .jnew("moa/gui/clustertab/ClusteringAlgoPanel")
    strm <- .jcall(panel, "Lmoa/streams/clustering/ClusteringStream;", "getStream")
    .jcall(strm, "V", "prepareForUse")
-
-   #RandomRBFGeneratorEvents extends abstract class ClusteringStream
-   #strm <- .jnew("moa/streams/clustering/RandomRBFGeneratorEvents")
-   #.jcall(strm, "V", "prepareForUse")
    
    #looping through the stream, feeding the new datapoints into 
    #the algorithm
    for (i in 1:1000) {
-
-      #TODO: this call isn't working, but when calling
-      # .jmethods(strm), the method signature does indeed exist
       inst <- .jnew("weka/core/Instance")
       inst <- .jcall(strm, "Lweka/core/Instance;", "nextInstance")
       .jcall(c, "V", "trainOnInstanceImpl", inst)
@@ -56,9 +68,3 @@ cluster <- function(clusterer) {
 	
 
 }
-
-#setMethod("cluster", signature(),
-#	function() {
- #          .jinit()
-#	}
-#)
