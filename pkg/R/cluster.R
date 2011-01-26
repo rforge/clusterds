308db90eb4b5b9490ef91ac3279d5a507309d68d
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-26 17:39:54 +0000

    fixed is.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cluster.R
+++ pkg/R/cluster.R
@@ -9,10 +9,14 @@ cluster <- function(clusterer, x, n=10000) {
     
     #TODO: how do we check if either of the classes == javaDS ??
     # casting the inst to a java object
-    if (class(x) != "DSD") {
+    if (!is(x, "DSD_moa")) {
       inst <- .jnew("weka/core/Instance", 1, inst)
     }
 
     .jcall(clusterer$javaObj, "V", "trainOnInstanceImpl", inst)
   }
+
+  ### so cl <- cluster(cl, ...) also works
+  invisible(clusterer)
+
 }
