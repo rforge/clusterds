55c1d6b99c9656f8bbd24cb829a0c3bbadfb2ea0
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-02 02:44:31 +0000

    fixed the bug related to the cluster separation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DSD_Static.R
+++ pkg/R/DSD_Static.R
@@ -1,18 +1,40 @@
 # clusterProb is a vector of probabilities for the clusters created
-DSD_Static <- function(mu=c(0.5,0.5), sd=c(0.2,0.2), clusterProb=1) {
-  
-  mu <- as.matrix(mu)
-  sd <- as.matrix(sd)
+DSD_Static <- function(n=1, dims=2, ...) {
 
-  if (ncol(mu) != ncol(sd) ||
-      nrow(mu) != nrow(sd)) {
-    stop("mu matrix is a different size than sd matrix")
+  #TODO: need to look into how to handle the list
+  # optional params
+  # mu=c(0.5,0.5), sd=c(0.2,0.2), clusterProb=1
+  
+#  mu <- as.matrix(mu)
+#  sd <- as.matrix(sd)
+
+#  if (ncol(mu) != ncol(sd) ||
+#      nrow(mu) != nrow(sd)) {
+#    stop("mu matrix is a different size than sd matrix")
+#  }
+
+  # if clusterProb isn't defined, we give all the clusters equal probability
+  if (clusterProb == NULL) {
+    prob <- 1/n
+    clusterProb <- as.vector(array(prob, n))
   }
 
-  # if the cluster probability differs in length, we default to an equal probability to all clusters
-  if (length(clusterProb) != ncol(mu)) {
-    prob <- 1/ncol(mu)
-    clusterProb <- as.vector(array(prob, ncol(mu)))
+  #TODO: need to randomly generate mu values
+  if (mu == NULL) {
+    mu <- matrix(ncol=dims, nrow=n)
+  }
+
+  #TODO: need to randomly generate sd values
+  if (sd == NULL) {
+    sd <- matrix(ncol=dims, nrow=n)
+  }
+
+  if (n == 1) {
+
+  } else if (n > 1) {
+    
+  } else {
+    stop("invalid n")
   }
 
   l <- list(Description = "Static R Data Stream",
@@ -28,18 +50,19 @@ getPoints.DSD_Static <- function(x, n=1, ...) {
   # if n == 1, we just create 1 instance
   if (n == 1) {
       numCluster <- sample(x=c(1:ncol(x$mu)), size=n, replace=TRUE, prob=x$clusterProb)
-      inst <- rnorm(n=nrow(x$mu), mean=x$mu[numCluster,], x$sd[numCluster,])
+      inst <- rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
   
   # otherwise, we loop and create a number of instances
   } else if (n > 1) {
       numCluster <- sample(x=c(1:ncol(x$mu)), size=n, replace=TRUE, prob=x$clusterProb)
-      inst <- rnorm(n=nrow(x$mu), mean=x$mu[numCluster,], x$sd[numCluster,])
+      inst <- rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
 
     #TODO: is there a better way to do this than looping? (using vectorization)
     for (i in 2:n) {
-      numCluster <- sample(x=c(1:ncol(x$mu)), size=n, replace=TRUE, prob=x$clusterProb)
-      inst <- rbind(inst, rnorm(n=ncol(x$mu), mean=x$mu[numCluster,], x$sd[numCluster,]))
+      inst <- rbind(inst, rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[i]],], x$sd[numCluster[[i]],]))
     }
+
+  # error out
   } else {
     stop("invalid n")
   }
