d23d6945935b3ab7b71d5a6d80fa981849defd20
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-02 03:36:39 +0000

    deleted old clusterer file, fixed a bug in the static stream, renamed some cluster vars
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DSD_Static.R
+++ pkg/R/DSD_Static.R
@@ -1,33 +1,30 @@
 # clusterProb is a vector of probabilities for the clusters created
-DSD_Static <- function(n=1, dims=2, ...) {
+#TODO: need a way to handle the list of params as optional (mu, sd, prob)
+DSD_Static <- function(n=1, dims=2, mu=c(.5,.5),sd=c(.2,.2),clusterProb=1) {
+ 
+  mu <- as.matrix(mu)
+  sd <- as.matrix(sd)
 
-  #TODO: need to look into how to handle the list
-  # optional params
-  # mu=c(0.5,0.5), sd=c(0.2,0.2), clusterProb=1
-  
-#  mu <- as.matrix(mu)
-#  sd <- as.matrix(sd)
-
-#  if (ncol(mu) != ncol(sd) ||
-#      nrow(mu) != nrow(sd)) {
-#    stop("mu matrix is a different size than sd matrix")
-#  }
+  if (ncol(mu) != ncol(sd) ||
+      nrow(mu) != nrow(sd)) {
+    stop("mu matrix is a different size than sd matrix")
+  }
 
   # if clusterProb isn't defined, we give all the clusters equal probability
-  if (clusterProb == NULL) {
+  #if (clusterProb == NULL) {
     prob <- 1/n
     clusterProb <- as.vector(array(prob, n))
-  }
+  #}
 
   #TODO: need to randomly generate mu values
-  if (mu == NULL) {
-    mu <- matrix(ncol=dims, nrow=n)
-  }
+  #if (mu == NULL) {
+  #  mu <- matrix(ncol=dims, nrow=n)
+  #}
 
   #TODO: need to randomly generate sd values
-  if (sd == NULL) {
-    sd <- matrix(ncol=dims, nrow=n)
-  }
+  #if (sd == NULL) {
+  #  sd <- matrix(ncol=dims, nrow=n)
+  #}
 
   if (n == 1) {
 
@@ -50,16 +47,16 @@ getPoints.DSD_Static <- function(x, n=1, ...) {
   # if n == 1, we just create 1 instance
   if (n == 1) {
       numCluster <- sample(x=c(1:ncol(x$mu)), size=n, replace=TRUE, prob=x$clusterProb)
-      inst <- rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
+      inst <- rnorm(n=nrow(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
   
   # otherwise, we loop and create a number of instances
   } else if (n > 1) {
       numCluster <- sample(x=c(1:ncol(x$mu)), size=n, replace=TRUE, prob=x$clusterProb)
-      inst <- rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
+      inst <- rnorm(n=nrow(x$mu), mean=x$mu[numCluster[[1]],], x$sd[numCluster[[1]],])
 
     #TODO: is there a better way to do this than looping? (using vectorization)
     for (i in 2:n) {
-      inst <- rbind(inst, rnorm(n=ncol(x$mu), mean=x$mu[numCluster[[i]],], x$sd[numCluster[[i]],]))
+      inst <- rbind(inst, rnorm(n=nrow(x$mu), mean=x$mu[numCluster[[i]],], x$sd[numCluster[[i]],]))
     }
 
   # error out
--- pkg/R/cluster.R
+++ pkg/R/cluster.R
@@ -1,31 +1,29 @@
-cluster <- function(clusterer, x, n=10000) { 
-    if (n < 2)
-	stop("numPoints must be > 1")
+cluster <- function(dsc, dsd, n=10000) { 
+  if (n < 2)
+    stop("numPoints must be > 1")
 
-    # looping through the stream, feeding the new datapoints into 
-    # the algorithm
-    for (i in 1:n) {
-	inst <- getPoints(x)
+  # looping through the stream, feeding the new datapoints into 
+  # the algorithm
+  for (i in 1:n) {
+    inst <- getPoints(dsd)
+    if(dsc$javaObj != NULL) {
 
-	# is the clusterer in java?
-	if(clusterer@javaObj != NULL) {
-	    # java clusterer
+      # casting the inst to a java object
+      if (!is(dsd, "DSD_moa")) {
+        inst <- .jnew("weka/core/Instance", 1, inst)
+      }
 
-	    # casting the inst to a java object
-	    if (!is(x, "DSD_moa")) {
-		inst <- .jnew("weka/core/Instance", 1, inst)
-	    }
+      .jcall(dsc$javaObj, "V", "trainOnInstanceImpl", inst)
 
-	    .jcall(clusterer$javaObj, "V", "trainOnInstanceImpl", inst)
-	}else{
-	    # R clusterer
-	    # FIXME: this needs more thinking! Maybe we should use a environment
-	    # to simulate pass by reference.
-	    clusterer <- clusterer$clusterFun(clusterer, inst)
-	}
+    } else {
+
+      # R clusterer
+      # FIXME: this needs more thinking! Maybe we should use a environment
+      # to simulate pass by reference.
+      clusterer <- dsc$clusterFun(dsc, inst)
     }
+  }
 
     ### so cl <- cluster(cl, ...) also works
-    invisible(clusterer)
-
+    invisible(dsc)
 }
