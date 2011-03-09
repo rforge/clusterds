2147eefb144f80d484202b51758e2a5a0cac068f
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-09 19:07:03 +0000

    added Work directory.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Work/test_tNN.R
@@ -0,0 +1,28 @@
+library("ClusterDS")
+
+#source("AAA.R")
+#source("cluster.R")
+#source("tNN.R")
+
+## create stream
+dsd <- DSD_Static(k=2, d=2, mu=rbind(c(0,0),c(1,1)))
+
+d <- getPoints(dsd, 1000, assignment = TRUE)
+
+plot(d, col=attr(d, "assignment"))
+pairs(d, col=attr(d, "assignment"))
+
+## cluster
+x <- DSC_tNN(, threshold=.4)
+
+cluster(x, dsd, 1000)
+
+pairs(get_centers(x))
+
+nclusters(x)
+plot(x)
+
+# check if the clusters are on top of the points
+plot(getPoints(dsd, 1000), col="grey")
+points(getCenters(x), col="red", pch=3)
+
