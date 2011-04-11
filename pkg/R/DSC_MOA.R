cfdc2c815ba3cba25751fb0b2775397bc5e0a53c
johnhforrest <johnhforrest@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-11 18:05:06 +0000

    updated the vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DSC_MOA.R
+++ pkg/R/DSC_MOA.R
@@ -29,28 +29,24 @@ get_centers.DSC_MOA <- function(x, ...) {
 
 	if (length < 2)
 	    stop("not enough microclusters, please cluster more data")
-
-	# this matrix will hold the data to plot
-	# TODO: ncol is tied to the number of dimensions
-	m <- matrix(ncol=2, nrow=length)
-	colnames(m) <- c("x", "y")
+	
+	# prepping before the loop
+	mCluster <- .jcall(mClusters, "Ljava/lang/Object;", "get", 0L)
+	mCluster <- .jcast(mCluster, "Lmoa/cluster/Cluster")
+	center <- .jcall(mCluster, "[D", "getCenter")
+	
+	m <- matrix(ncol=length(center), nrow=length)
+	m[1,] <- center
 
 	# iterating over the array, extracting data to be plotted
-	for (i in 1:length) {
+	# the first point has already been used, so start from 2
+	for (i in 2:length) {
 
 	    # will have to cast mCluster as moa/cluster/Cluster
 	    mCluster <- .jcall(mClusters, "Ljava/lang/Object;", "get", i-1L)
 	    mCluster <- .jcast(mCluster, "Lmoa/cluster/Cluster")
-
-	    # array of doubles for each dimension
 	    center <- .jcall(mCluster, "[D", "getCenter") 
-
-	    #TODO: projection
-	    # if the data is 2 dimensional, we don't have to project 
-	    #if (length(center) == 2) {
-	    m[i, 1] = center[1]
-	    m[i, 2] = center[2]
-	    #}
+	    m[i,] <- center
 	}
 
 	# returning the matrix 
