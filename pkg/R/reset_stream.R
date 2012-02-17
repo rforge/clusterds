429da2c0699358978a4f71146b12313e72840dd6
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-17 17:54:14 +0000

    fixed man pages.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/reset_stream.R
+++ pkg/R/reset_stream.R
@@ -2,7 +2,7 @@ reset_stream.default <- function(dsd) {
     stop(gettextf("reset_stream not implemented for class '%s'.", class(dsd)))
 }
 
-reset_stream <- function(x) UseMethod("reset_stream")
+reset_stream <- function(dsd) UseMethod("reset_stream")
 
 reset_stream.DSD_Wrapper <- function(dsd) {
     dsd$state$counter <- 1
