4269c6bec99653bfcc4bf8fe2aa9564c226ad14d
mbolanos <mbolanos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-14 15:22:10 +0000

    prune
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/prune_clusters.R
+++ pkg/R/prune_clusters.R
@@ -1,9 +1,13 @@
-prune_clusters <- function(dsc, prop=.05) {
+prune_clusters <- function(dsc, threshold=.05, prob=TRUE) {
 
 	 w <- get_weights(dsc, scale=NULL)
 	 
 	 o <- order(w)
-	 o <- o[w[o] > quantile(w,prob=prop)]
+	 
+	 if(prob)
+	 	o <- o[w[o] > quantile(w,prob=threshold)]
+	 else
+	 	o <- head(o,length(o)*(1-threshold))
 
 	 DSC_Static(get_centers(dsc)[o,],get_microclusters(dsc),w[o])
 }
