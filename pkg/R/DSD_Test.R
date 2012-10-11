842cdbb6e20f406a1f7c415dbfecd249dd73494f
mbolanos <mbolanos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-11 21:31:33 +0000

    test.r
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@145 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/DSD_Test.R
@@ -0,0 +1,74 @@
+DSD_Test <- function(noise = 0) {
+
+
+
+    # creating the DSD object
+    l <- list(description = "Test Shapes",
+	    d = 2,
+	    k = 4,
+	    noise = noise)
+    class(l) <- c("DSD_Test","DSD_R","DSD")
+    l
+}
+
+get_points.DSD_Test <- function(x, n=1, assignment = FALSE,...) {
+    ### gaussians at (3,2.5) and (3,-2.5)
+### bars at (-3,2.8) and (-3,-2.8)
+
+	
+	df <- data.frame()
+        a <- numeric()
+
+	for(i in 1:n) {
+	
+	type <- sample(0:4, 1, prob=c(x$noise,.5*(1-x$noise),.25*(1-x$noise),.5*(1-x$noise),.25*(1-x$noise)))
+	
+	if(type==0) {
+		p <- runif(2, -6,6)
+	}
+	
+	if(type==1) {
+		### gaussian
+		cen <- c(3,2.5)
+		p <- rnorm(2)
+		p <- p+cen
+	}
+	
+	if(type==2) {
+		### gaussian
+		cen <- c(3,-2.5)
+		p <- rnorm(2)
+		p <- p+cen
+	}
+	
+	if(type==3) {
+		### bar
+		cen <- c(-3, 2.8)
+		hight <- 5
+		p <- c(runif(1, -1,1), runif(1, -hight/2, hight/2))
+		p <- p+cen
+	}
+	
+	if(type==4) {
+		### bar
+		cen <- c(-3, -2.8)
+		hight <- 5
+		p <- c(runif(1, -1,1), runif(1, -hight/2, hight/2))
+		p <- p+cen
+	}
+	
+	if(assignment)
+		a <- c(a,type)
+	df <- rbind(df,p)
+	
+	}
+	
+	
+	if(assignment) {
+		attr(df,"assignment")<-a
+	}
+	
+	names(df) <- 1:ncol(df)
+	
+	df
+}
