03c0bca38540c7c91873a495e81dec635a74bc59
mbolanos <mbolanos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-21 19:34:41 +0000

    added reset attribute
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stream/R/MGC_Linear.R
+++ pkg/stream/R/MGC_Linear.R
@@ -16,7 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-add_keyframe <- function(x, time, variance = 1, density = 1, center, cluster = NA) 
+add_keyframe <- function(x, time, variance = 1, density = 1, center, cluster = NA, reset = FALSE) 
   UseMethod("add_keyframe")
 get_keyframes <- function(x) 
   UseMethod("get_keyframes")
@@ -26,13 +26,15 @@ remove_keyframe <- function(x, time)
 MGC_Linear_refClass <- setRefClass("MGC_Linear", 
                             fields = list(
                               keyframes = "data.frame",
-                              dimension = "numeric"
+                              dimension = "numeric",
+                              reset_time = "numeric"
                             ), 
                             
                             methods = list(
                               initialize = function() {
-                                keyframes  <<- data.frame(time =  numeric(0),variance = list(), density = numeric(0), cluster = numeric(0), centers = list())
+                                keyframes  <<- data.frame(time =  numeric(0),variance = list(), density = numeric(0), cluster = numeric(0), centers = list(), reset = logical())
                                 dimension <<- 0
+                                reset_time <<- 0
                                 .self
                               }
                               
@@ -40,9 +42,9 @@ MGC_Linear_refClass <- setRefClass("MGC_Linear",
 )
 
 MGC_Linear_refClass$methods(
-  add_keyframe = function(t,v,d,p,c) {
+  add_keyframe = function(t,v,d,p,c,r) {
     dimension <<- length(p)
-    keyframes <<- rbind(keyframes,data.frame(time=t,variance=I(list(v)),density=d,cluster=c,centers=I(list(p))))
+    keyframes <<- rbind(keyframes,data.frame(time=t,variance=I(list(v)),density=d,cluster=c,centers=I(list(p)),reset=r))
     keyframes <<- keyframes[with(keyframes, order(time)), ]
   },
   get_points = function(time) {
@@ -50,10 +52,15 @@ MGC_Linear_refClass$methods(
     MASS::mvrnorm(1, mu=unlist(attributes[[1]]), Sigma=diag(unlist(attributes[[2]]),dimension))
   },
   get_attributes = function(time) {
+    test <- keyframes$reset[which(keyframes$time==floor(time-reset_time))]
+    if(length(test) != 0 && test)
+      reset_time <<- time
+    
     get_attribute(time,c("cluster","density"))
+    
   },
   get_attribute = function(time,attributes) {
-    do.call(rbind, lapply(time,function(t){
+    do.call(rbind, lapply((time-reset_time),function(t){
       outer <- findInterval(t, c(-Inf, keyframes$time))
       inner <- outer -1
       if(outer==1) {
@@ -70,7 +77,7 @@ MGC_Linear_refClass$methods(
         if(attribute == "cluster") return(get(attribute,keyframes)[inner])
         (unlist(get(attribute,keyframes)[inner])-unlist(get(attribute,keyframes)[outer]))/(keyframes$time[inner]-keyframes$time[outer])*(t-keyframes$time[inner])+unlist(get(attribute,keyframes)[inner]) 
       })))
-    }))  
+    })) 
   }
 )
 
@@ -85,9 +92,9 @@ MGC_Linear<- function() {
             class = c("MGC_Linear","MGC"))
 }
 
-add_keyframe.MGC_Linear <- function(x, time, variance = 1, density = 1, center, cluster = NA) {
+add_keyframe.MGC_Linear <- function(x, time, variance = 1, density = 1, center, cluster = NA, reset = FALSE) {
   x$RObj$keyframes <- x$RObj$keyframes[which(x$RObj$keyframes$time!=time),]
-  x$RObj$add_keyframe(time,variance,density, center, cluster)
+  x$RObj$add_keyframe(time,variance,density, center, cluster, reset)
 }
 
 get_keyframes.MGC_Linear <- function(x) {
