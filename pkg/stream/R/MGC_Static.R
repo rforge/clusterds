e42b7b69453afa1f7a4d306fca4847805c63430c
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-22 02:16:43 +0000

    added some files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clusterds@241 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/stream/R/MGC_Static.R
@@ -0,0 +1,27 @@
+#######################################################################
+# Moving Generator -  Infrastructure for Moving Streams
+# Copyright (C) 2013 Michael Hahsler, Matthew Bolanos, John Forrest 
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+### creator    
+MGC_Static <- function(density = 1, variance = 1, center, cluster = NA) {
+    x <- MGC_Linear(list(keyframe(0, density=density, variance=variance, 
+		 center=center, cluster=cluster)))
+    
+    class(x) <- c("MGC_Static", class(x))
+    x
+}
+
